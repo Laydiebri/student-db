aec03d0080f33360a280c886f95fa0f956c2cdb1
Zabrina Osborne <Laydiebri24@hotmail.com>
2021-04-27 17:58:19 +0100

    adding delete

--- static/all_modules.js
+++ static/all_modules.js
@@ -4,8 +4,18 @@
 var mainApp = angular.module("mainApp", []);
 
 // Create the controller
-mainApp.controller("moduleController", function($scope, $http) {
-    $http.get('/modules').then(function(response) {
+  mainApp.controller("moduleController", function($scope, $http) {
+    $http.get("/modules").then(function(response) {
       $scope.modules = response.data;
     });
+    // Sends a delete message to the server
+    $scope.deleteModule = function(code) {
+      // Send delete message to /module/code
+      $http.delete("/module/" + code).then(function(response) {
+        // When request completes, refresh list of modules
+        $http.get("/modules").then(function(response) {
+          $scope.modules = response.data;
+        });
+      });
+    };
   });
--- static/all_students.js
+++ static/all_students.js
@@ -5,7 +5,19 @@ var mainApp = angular.module("mainApp", []);
 
 // Create the controller
 mainApp.controller("studentController", function($scope, $http) {
+    // Initially hides the "selected" element
+    document.getElementById("selected").style.display="none";
+  
     $http.get("/students").then(function(response) {
       $scope.students = response.data;
     });
+  
+    $scope.selectStudent = function(id) {
+      // Get student by id
+      $http.get("/student/" + id).then(function(response) {
+        $scope.selectedStudent = response.data;
+        // Show the selected element
+        document.getElementById("selected").style.display="block";
+      });
+    }
   });
