c360ca50f188b8596f91c0316b21aec8b9aa53fd
Zabrina Osborne <Laydiebri24@hotmail.com>
2021-04-27 17:42:29 +0100

    Selecting data 1

--- static/all_programmes.js
+++ static/all_programmes.js
@@ -5,7 +5,19 @@ var mainApp = angular.module("mainApp", []);
 
 // Create the controller
 mainApp.controller("programmeController", function($scope, $http) {
+    // Initially hides the "selected" element
+    document.getElementById("selected").style.display="none";
+  
     $http.get("/programmes").then(function(response) {
       $scope.programmes = response.data;
     });
+  
+    $scope.selectProgramme = function(code) {
+      // Get specific programme by code
+      $http.get("/programme/" + code).then(function(response) {
+        $scope.selectedProgramme = response.data;
+        // Show the "selected" element
+        document.getElementById("selected").style.display="block";
+      });
+    }
   });
--- static/all_students.js
+++ static/all_students.js
@@ -5,8 +5,7 @@ var mainApp = angular.module("mainApp", []);
 
 // Create the controller
 mainApp.controller("studentController", function($scope, $http) {
-  $scope.students = [
-    new Student("001", "Test", "Test", new Programme("001", "Test")),
-    new Student("002", "Tester", "Tester", new Programme("002", "Test 2"))
-  ];
-});
+    $http.get("/students").then(function(response) {
+      $scope.students = response.data;
+    });
+  });
